406101cd29afccaceb619b602988108ea309e01a
leroy <kerettsleroy@gmail.com>
2024-06-28 16:13:18 +0300

    first commit

---
+++ Desktop/Mini Project/STRATHFLIX/src/store/index.js
@@ -0,0 +1,15 @@
+import Vuex from'vuex'
+
+import products from './modules/products'
+import cart from './modules/cart';
+export default new Vuex.Store({
+    state: {},
+    getters: {},
+    actions: {},
+    mutations: {},
+    modules: {
+        products,
+        cart
+
+    }
+})
